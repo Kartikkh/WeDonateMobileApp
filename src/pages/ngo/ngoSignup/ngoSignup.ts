78e4968c552a7b079a424dbacc1e4e56b08375d6
vishal isharani <isharanivishal@gmail.com>
2017-11-08 14:00:00 +0530

    Vishal | ngo create account reactive form

--- src/pages/ngo/ngoSignup/ngoSignup.ts
+++ src/pages/ngo/ngoSignup/ngoSignup.ts
@@ -1,35 +1,43 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
-import {NgForm} from "@angular/forms";
-import {Http} from '@angular/http';
-import {Constants} from '../../../constant/constant';
+import { FormBuilder, FormGroup, NgForm, Validators } from '@angular/forms';
+import { Http } from '@angular/http';
+import { Constants } from '../../../constant/constant';
 import { LoadingController } from 'ionic-angular';
 
 
 @Component({
   selector: 'page-users',
-  templateUrl: 'ngoSignup.html',
+  templateUrl: 'ngoSignup.html'
 })
 export class ngoSignupPage {
+  ngoForm: FormGroup;
 
-  constructor(public navCtrl: NavController,
-              public navParams: NavParams,
-              public loadingCtrl: LoadingController,
-              public http : Http) {}
+  constructor(public loadingCtrl: LoadingController,
+              public http: Http, private _fb: FormBuilder) {
+    this.ngoForm = this._fb.group({
+      ngoName: ['', Validators.required],
+      regNo: ['', Validators.required],
+      password: ['', Validators.required],
+      email: ['', Validators.compose([Validators.required, Validators.email])],
+      contactNo: ['', Validators.required],
+      address: ['', Validators.required]
+    });
+  }
 
-  ngoSignUp(form : NgForm ){
+  ngoSignUp() {
     const loading = this.loadingCtrl.create({
       content: 'Please wait...'
     });
     loading.present();
-   this.http.post(Constants.ngoSignup() , form.value).subscribe(
-     data=>{
-       loading.dismiss();
-          console.log(data);
-   }, err => {
-       loading.dismiss();
-     console.log("Error occured.")
-   })
+    this.http.post(Constants.ngoSignup(), this.ngoForm.value).subscribe(
+      data => {
+        loading.dismiss();
+        console.log(data);
+      }, err => {
+        loading.dismiss();
+        console.log('Error occured.')
+      })
   }
 
 
